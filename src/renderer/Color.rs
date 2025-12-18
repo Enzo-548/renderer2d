52eab886dcf047b86479b1a2a5e10496fa481f38
Enzo Picoli <enzosalatinopicoli@gmail.com>
2025-12-18 15:28:36 -0300

    new colors added

--- src/renderer/Color.rs
+++ src/renderer/Color.rs
@@ -7,10 +7,31 @@ pub struct Color{
 }
 
 impl Color{
+    pub const RED: Color   = Color { r: 255, g: 0,   b: 0,   a: 255 };
+    pub const GREEN: Color = Color { r: 0,   g: 255, b: 0,   a: 255 };
+    pub const BLUE: Color  = Color { r: 0,   g: 0,   b: 255, a: 255 };
+    pub const BLACK: Color = Color { r: 0,   g: 0,   b: 0,   a: 255 };
+    pub const WHITE: Color = Color { r: 255, g: 255, b: 255, a: 255 };
+
     pub fn new(r:u8,g:u8,b:u8,a:u8) -> Self{
         Self {r,g,b,a}
     }
     pub fn zero() -> Self{
         Self{r:0,g:0,b:0,a:0}
+    }
+    pub fn full() -> Self{
+        Self{r:255,g:255,b:255,a:255}
+    }
+    pub fn red() -> Self{
+        Color::RED
+    }
+    pub fn green() -> Self{
+        Color::GREEN
+    }pub fn blue() -> Self{
+        Color::BLUE
+    }pub fn white() -> Self{
+        Color::WHITE
+    }pub fn black() -> Self{
+        Color::BLACK
     }
 }
