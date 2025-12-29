0cee1e6f4866f774387feff8899465f0f58a4be1
Enzo Picoli <enzosalatinopicoli@gmail.com>
2025-12-29 00:41:50 -0300

    new color added

--- src/renderer/Color.rs
+++ src/renderer/Color.rs
@@ -1,4 +1,4 @@
-#[derive (Debug, Clone, Copy)]
+#[derive (Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Color{
     pub r: u8,
     pub g: u8,
@@ -12,7 +12,7 @@ impl Color{
     pub const BLUE: Color  = Color { r: 0,   g: 0,   b: 255, a: 255 };
     pub const BLACK: Color = Color { r: 0,   g: 0,   b: 0,   a: 255 };
     pub const WHITE: Color = Color { r: 255, g: 255, b: 255, a: 255 };
-
+    pub const ZERO: Color  = Color { r:0,    g:0,    b:0,    a:0    };
     pub fn new(r:u8,g:u8,b:u8,a:u8) -> Self{
         Self {r,g,b,a}
     }
